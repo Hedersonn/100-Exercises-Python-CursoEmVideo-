5d88ca4789a88824fa321dd67983b886c3058a2d
Hedersonn <rigonatti.hederson@gmail.com>
2025-04-27 21:27:42 -0300

    Update Ex40.py

--- World_2/Ex40.py
+++ World_2/Ex40.py
@@ -4,6 +4,21 @@
 # Média entre 5.0 e 6.9: RECUPERAÇÃO
 # Média 7.0 ou superior: APROVADO
 
+#Cores
+green = '\033[32m'
+yellow = '\033[33m'
+red = '\033[31m'
+clear = '\033[m'
+
 #Valores
 nota1 = float(input('Primeira nota: '))
 nota2 = float(input('Segunda nota: '))
+media = (nota1 + nota2) / 2
+
+#Condições
+if media < 5.0:
+    print(red + f' MEDIA DE {media:.1f}, REPROVADO!' + clear)
+elif media < 7.0:
+    print(yellow + f'MEDIA DE {media:.1f}, RECUPERAÇÃO' + clear)
+else:
+    print(green + f'MEDIA DE {media:.1f}, APROVADO' + clear)
